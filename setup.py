5d70fbd3b11bf4bbe29220deb56b8f69a0765838
Hemesh2004 <hemesh4445@gmail.com>
2026-06-13 16:49:08 +0530

    Requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Hemesh Meka",
+    author_email="hemesh44445@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
